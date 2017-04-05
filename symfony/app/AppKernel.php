aebd6c4eaa5f907f5a8dc61b5adae93ac0aaf3ac
Jose Neves <jnevesju@its.jnj.com>
2017-04-05 11:05:53 -0300

    Add bundles

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -16,7 +16,13 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
             new SampleBundle\BaseBundle\BaseBundle(),
+
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- symfony/src/SampleBundle/BaseBundle/Controller/DefaultController.php
+++ symfony/src/SampleBundle/BaseBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BaseBundle:Default:index.html.twig');
+        return ['Hello World!'];
     }
 }
