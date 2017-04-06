a6c9a650364b6665c6237e55201ad4f0e21ac05c
Jose Neves <jnevesju@its.jnj.com>
2017-04-06 15:40:42 -0300

    yo angular router

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -11,8 +11,27 @@
 angular
   .module('angularApp', [
     'ngAnimate',
+    'ngAria',
     'ngCookies',
+    'ngMessages',
     'ngResource',
+    'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ]);
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
