cf4e7a177a4c66f0a1c07f00005479381bc7372f
Jose Neves <jnevesju@its.jnj.com>
2017-04-06 13:10:15 -0300

    Add Yeoman

---
+++ angular/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
