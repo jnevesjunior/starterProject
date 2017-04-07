9fe6724c988d5288b686666d5614131139ea7b42
Jose Neves <jnevesju@its.jnj.com>
2017-04-07 08:34:30 -0300

    add angular material

--- angular/app/scripts/app.js
+++ angular/app/scripts/app.js
@@ -9,29 +9,40 @@
  * Main module of the application.
  */
 angular
-  .module('angularApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('angularApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ngMaterial'
+    ])
+    .config(function ($routeProvider)
+            {
+                $routeProvider
+                    .when('/', {
+                        templateUrl : 'views/main.html',
+                        controller  : 'MainCtrl',
+                        controllerAs: 'main'
+                    })
+                    .when('/about', {
+                        templateUrl : 'views/about.html',
+                        controller  : 'AboutCtrl',
+                        controllerAs: 'about'
+                    })
+                    .otherwise({
+                                   redirectTo: '/'
+                               });
+            })
+    .config(function ($mdThemingProvider)
+            {
+                $mdThemingProvider.theme('default')
+                    .primaryPalette('blue', {
+                        'default': '900'
+                    });
+                $mdThemingProvider.theme('green')
+                    .primaryPalette('green');
+            });
